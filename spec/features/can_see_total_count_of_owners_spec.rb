8fe2400af21f132e360c27c53441c804ab285dec
Ken Lee <wewert@gmail.com>
2017-04-21 14:48:39 -0600

    Don't know why a super old version uploaded first. Now fix

--- spec/features/can_see_total_count_of_owners_spec.rb
+++ spec/features/can_see_total_count_of_owners_spec.rb
@@ -1,13 +1,16 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "User visits page" do
-  scenario "and sees total count of owners by cleaner next to their name" do
-    @owners = @cleaner.owners.create(first_name: "Jessica", last_name: "Jones")
-    @cleaner = Cleaner.create(company_name: "Green")
+RSpec.feature "User visits owners/:id page" do
+  scenario "user sees the cleaners the owner employs to clean rentals" do
+    @owner = Owner.create(first_name: "Jessica", last_name: "Jones")
+    @cleaner1 = Cleaner.create(type: "Green")
+    @cleaner2 = Cleaner.create(type: "Blue")
 
-  visit owners_path
+    @oc = owner.cleaners << cleaner1, cleaner2
 
-  expect(page).to have_content("Green has 2 employers")
-  expect(page).to have_content("Blue has 2 employers")
+    visit owner_cleaners_path
+
+    expect(page).to have_content("Jessica")
+    expect(page).to have_content("Jones")
   end
 end
